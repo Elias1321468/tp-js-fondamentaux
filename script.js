a44b773dc25ef1929fa4f82388b2009e3f8d5d47
Elias1321468 <teanodupont@gmail.com>
2025-11-04 08:07:58 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 console.log("Laboratoire prêt !");
 const nom = "Leroy";
-let age = 17;
+let age = 18;
+console.log(nom);
+console.log(age); 
+
+const motDePasseAttendu = "secret123";
+let motDePasseUtilisateur = "secret123";
+if (motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+    }
+else if (<=8) {
+    console.log(8<);
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
+
+
